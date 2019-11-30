416625d2573f39013ed1f4eb40e42e454505c012
HarunBuyuktepe <harun.buyuktepe@gmail.com>
2019-11-30 20:58:56 +0300

    uniform cost search added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
-import org.json.simple.JSONObject;
-import org.omg.CORBA.MARSHAL;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
 
+    public static int costUCS;
+    public static int costDFS;
+    public static int costBFS;
+    public static int costIDS;
+    public static int costAstar;
+    public static int costGreedy;
     public static void main(String[] args) {
         System.out.println("Harun Baba");
 
@@ -17,6 +20,7 @@ public class Main {
         DFS dfs = new DFS();
         ArrayList<LabyrinthNodes> ExploredSetDFS = dfs.applyDFS(maze,17);
         Iterator<LabyrinthNodes> iterator = ExploredSetDFS.iterator();
+        System.out.println("Cost of Uniform Cost Search" + " " + costDFS);
         while (iterator.hasNext()) {
             System.out.println(iterator.next().toString());
         }
@@ -25,6 +29,7 @@ public class Main {
         BFS bfs = new BFS();
         ArrayList<LabyrinthNodes> ExploredSetBFS = dfs.applyDFS(maze,17);
         Iterator<LabyrinthNodes> iterator1 = ExploredSetDFS.iterator();
+        System.out.println("Cost of Uniform Cost Search" + " " + costBFS);
         while (iterator1.hasNext()) {
             System.out.println(iterator1.next().toString());
         }
@@ -33,8 +38,18 @@ public class Main {
         IDS ids = new IDS();
         ArrayList<LabyrinthNodes> ExploredSetIDS = ids.applyIDS(maze,17);
         Iterator<LabyrinthNodes> iterator2 = ExploredSetIDS.iterator();
+        System.out.println("Cost of Uniform Cost Search" + " " + costIDS);
         while (iterator2.hasNext()) {
             System.out.println(iterator2.next().toString());
+        }
+
+        System.out.println("\nIDS Explored Set");
+        UCS ucs = new UCS();
+        ArrayList<LabyrinthNodes> ExploredSetUCS = ucs.applyUCS(maze,17);
+        Iterator<LabyrinthNodes> iterator3 = ExploredSetUCS.iterator();
+        System.out.println("Cost of Uniform Cost Search" + " " + costUCS);
+        while (iterator3.hasNext()) {
+            System.out.println(iterator3.next().toString());
         }
     }
 }
